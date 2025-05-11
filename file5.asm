552c79f078ef86bbf31fb636ab875aaad9c3e0ad
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2025-05-11 08:34:06 +0500

    init

---
+++ file5.asm
@@ -0,0 +1,25 @@
+; Program to subtract two numbers
+
+dosseg
+.model small
+.stack 100h
+.data
+.code
+
+main proc
+mov bl, 3
+mov cl, 2
+
+sub bl, cl
+
+add bl, 48
+mov dl, bl
+
+mov ah, 2
+int 21h
+
+mov ah, 4ch
+int 21h
+
+main endp
+end main
