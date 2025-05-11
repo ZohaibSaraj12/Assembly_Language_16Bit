afb7fe3c81ecff4b47469305b8d157b2688637b3
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2025-05-11 08:28:06 +0500

    int

---
+++ file4.asm
@@ -0,0 +1,20 @@
+; Program to add two numbers
+
+.model small 
+.stack 100h
+.data
+.code
+main proc
+
+mov bl, 2
+add bl, 1
+mov dl, bl
+add dl, 48
+
+mov ah, 2
+int 21h
+
+mov ah, 4ch
+int 21h
+main endp
+end main
