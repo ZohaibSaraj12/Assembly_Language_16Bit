c957ffc2850df15f38bd9a0212e39da997082ca9
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2025-05-28 09:31:14 +0500

    Changes Implemented

--- file1.asm
+++ file1.asm
@@ -1,17 +1,34 @@
 ; Program to print a single character in MASM assembly 
-dosseg
-.model small
-.stack 100h
-.data
-.code 
+; dosseg
+; .model small
+; .stack 100h
+; .data
+; .code 
 
-main proc
+; main proc
+
+; mov dl, 97
+; mov ah, 2
+; int 21h
+; mov ah, 4ch
+; int 21h
+; main endp
+; end main
 
 
-mov dl, 97
-mov ah, 2
-int 21h
-mov ah, 4ch
-int 21h
-main endp
-end main
+
+.model small 
+.stack 100h    ; stack of size 100h which is enough to p
+.data   ; data section where variables are declared 
+.code  ; code section which tells that code starts from here 
+main proc   ; procedure --> Function 
+
+mov dl, 97    ; moving character into a data register
+mov ah, 2    ; service routine acts as a argument of function and ah is used for input and output
+int 21h      ; interrupt which tells the CPU to stop working and to print something, 
+
+mov ah, 4ch    ; now we are in register, to go outside of that register we have to give service routine of 4ch 
+int 21h       ; again interrupt to do that work, 
+
+main endp   ; end of the procedure main, 
+end main    ; starting of the main function 
